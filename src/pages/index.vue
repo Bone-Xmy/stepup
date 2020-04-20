0f2b47f8d8f7894bf3db602f13bf65a0d5c1627b
xiaomingye <xiaomingye@bone.com>
2020-04-20 08:18:10 +0800

    教学设备展示页面

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -12,9 +12,35 @@
            <div class="swiper-button-next" slot="button-next"></div>
          </swiper>
        </div>
-       <div class="ads-box"></div>
-       <div class="banner"></div>
-       <div class="product-box"></div>
+       <!-- <div class="ads-box">
+         <a :href="'/#/produce/' + item.id" v-for="(item, index) in adsList" :key="index">
+           <img :src="item.img" alt="">
+         </a>
+       </div> -->
+       <div class="banner">
+         <a :href="'/#/produce/30'">
+           <img src="/imgs/banner-1.png" alt="">
+         </a>
+       </div>
+       <div class="product-box">
+         <h2>教学设备一体机</h2>
+         <div class="wrapper">
+           <div class="list-box">
+             <div class="list" v-for="(arr, i) in deviceList" :key="i">
+               <div class="item" v-for="(item, j) in arr" :key="j">
+                 <div class="item-img">
+                   <img src="" alt="">
+                 </div>
+                 <div class="item-info">
+                   <h3>智恩教学</h3>
+                   <p>引进智能设备教学</p>
+                   <p>看过：10</p>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
      </div>
      <service-bar></service-bar>
     </div>
@@ -69,6 +95,24 @@
             id: '',
             img: '/imgs/slider/slide-5.jpg'
           }
+        ],
+        adsList: [
+         {
+           id: 33,
+           img: '/imgs/ads/ads-1.png'
+         }, {
+           id: 34,
+           img: '/imgs/ads/ads-2.jpg'
+         }, {
+           id: 35,
+           img: '/imgs/ads/ads-3.png'
+         }, {
+           id: 36,
+           img: '/imgs/ads/ads-4.jpg'
+         }
+        ],
+        deviceList: [
+
         ]
       }
     }
@@ -76,6 +120,8 @@
 </script>>
 
 <style lang="scss">
+  @import './../assets/scss/mixin.scss';
+
   .index {
     .swiper-box {
       .swiper-container {
@@ -85,6 +131,19 @@
           height: 100%;
         }
       }
+    }
+    // .ads-box {
+    //   @include flex();
+    //   margin-top: 14px;
+    //   margin-bottom: 31px;
+    //   a {
+    //     width: 296px;
+    //     height: 167px;
+    //   }
+    // }
+    .banner {
+      margin-top: 31px;
+      margin-bottom: 50px;
     }
   }
 </style>
