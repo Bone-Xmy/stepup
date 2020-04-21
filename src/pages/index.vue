2ebfb16d7b3a187352f5d76caf18fc9d9d1a1c6f
xiaomingye <xiaomingye@bone.com>
2020-04-21 08:07:01 +0800

    展示页修改

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,7 +4,7 @@
        <div class="swiper-box">
          <swiper :options="swiperOption">
            <swiper-slide v-for="(item, index) in slideList" :key="index">
-             <a :href="'/#/product' + item.id"><img :src="item.img"></a>
+             <a :href="'/#/product/' + item.id"><img :src="item.img"></a>
            </swiper-slide>
            <!-- Optional controls -->
            <div class="swiper-pagination" slot="pagination"></div>
@@ -22,27 +22,29 @@
            <img src="/imgs/banner-1.png" alt="">
          </a>
        </div>
-       <div class="product-box">
-         <h2>教学设备一体机</h2>
-         <div class="wrapper">
-           <div class="list-box">
-             <div class="list" v-for="(arr, i) in deviceList" :key="i">
-               <div class="item" v-for="(item, j) in arr" :key="j">
-                 <div class="item-img">
-                   <img src="" alt="">
-                 </div>
-                 <div class="item-info">
-                   <h3>智恩教学</h3>
-                   <p>引进智能设备教学</p>
-                   <p>看过：10</p>
-                 </div>
-               </div>
-             </div>
-           </div>
-         </div>
-       </div>
      </div>
-     <service-bar></service-bar>
+     <div class="product-box">
+      <div class="container">
+        <h2>教学设备一体机</h2>
+        <div class="wrapper">
+          <div class="list-box">
+            <div class="list" v-for="(arr, i) in deviceList" :key="i">
+              <div class="item" v-for="(item, j) in arr" :key="j">
+                <div class="item-img">
+                  <img :src="item.img" alt="">
+                </div>
+                <div class="item-info">
+                  <h3>智恩教学</h3>
+                  <p>引进智能设备教学</p>
+                  <p class="price">看过：10</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <service-bar></service-bar>
     </div>
 </template>>
 <script>
@@ -112,14 +114,44 @@
          }
         ],
         deviceList: [
-
+          [
+            {
+              id: 22,
+              img: '/imgs/ads/ads-1.png'
+            }, {
+              id: 22,
+              img: '/imgs/ads/ads-1.png'
+            }, {
+              id: 22,
+              img: '/imgs/ads/ads-1.png'
+            }, {
+              id: 22,
+              img: '/imgs/ads/ads-1.png'
+            }
+          ],[1,2,3,4]
         ]
+      }
+    },
+    mounted() {
+      this.init();
+    },
+    methods: {
+      init() {
+        this.axios.get('/products', {
+          params: {
+            categoryId: 100012,
+            pageSize: 8
+          }
+        }).then((res) => {
+          this.deviceList = [res.list.slice(0,4), res.list.slice(4,8)];
+        })
       }
     }
   }
 </script>>
 
 <style lang="scss">
+  @import './../assets/scss/config.scss';
   @import './../assets/scss/mixin.scss';
 
   .index {
@@ -144,6 +176,61 @@
     .banner {
       margin-top: 31px;
       margin-bottom: 50px;
+    }
+    .product-box {
+      background-color: $colorJ;
+      padding: 30px 0 50px;
+      h2 {
+        font-size: $fontF;
+        height: 21px;
+        line-height: 21px;
+        color: $colorB;
+        margin-bottom: 20px;
+      }
+      .wrapper {
+        // display: flex;
+        .list-box {
+          .list {
+            @include flex();
+            width: 1226px;
+            // margin-left: 20px;
+            margin-bottom: 14px;
+            &:last-child {
+              margin-bottom: 0;
+            }
+            .item {
+              width: 23.8%;
+              height: 302px;
+              background-color: $colorG;
+              text-align: left;
+              .item-img {
+                img {
+                  height: 238px;
+                  width: 100%;
+                }
+              }
+              .item-info {
+                h3 {
+                  font-size: $fontJ;
+                  color: $colorB;
+                  line-height: 14px;
+                  font-weight: bold;
+                }
+                p {
+                  color: $colorD;
+                  line-height: 13px;
+                  margin: 6px auto 8px;
+                }
+                .price {
+                  color: #f20a0a;
+                  font-size: $fontJ;
+                  font-weight: bold;
+                }
+              }
+            }
+          }
+        }
+      }
     }
   }
 </style>
